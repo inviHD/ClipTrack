d7f028aba0ce5fa727f03947c423ddf6aabd1f96
Markus Gräff <95489294+inviHD@users.noreply.github.com>
2025-04-16 23:31:31 +0200

    Delete build directory

--- build/.qt/QtDeployTargets-MinSizeRel.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_ClipTrack_FILE C:/Users/graef/source/repos/ClipTrack/build/MinSizeRel/ClipTrack.exe)
-set(__QT_DEPLOY_TARGET_ClipTrack_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_ClipTrack_RUNTIME_DLLS C:/Qt/6.8.1/msvc2022_64/bin/Qt6Widgets.dll;C:/Qt/6.8.1/msvc2022_64/bin/Qt6Gui.dll;C:/Qt/6.8.1/msvc2022_64/bin/Qt6Core.dll)
